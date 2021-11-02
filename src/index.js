5274d05db5024ae8ed7f524698896f6d6e53afc3
unity master <unitymasterx.com>
2021-11-01 20:19:29 -0500

    Agregaando Sass a nuestro proyecto

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom'
 import App from './routes/App'
 
 
+
+
 ReactDOM.render(
   <App/>,
   document.getElementById('app')
   
 )
-
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
 	entry: './src/index.js',
@@ -59,4 +59,4 @@ module.exports = {
 			filename: 'assets/[name].css',
 		}),
 	],
-}
+};
